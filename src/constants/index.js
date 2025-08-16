e557ae71c18076e0894afa49c0cde0be7a9c4834
Vinayak Yadav <netgoluyadav337@gmail.com>
2025-08-16 12:24:40 +0530

    Navbar added

--- src/constants/index.js
+++ src/constants/index.js
@@ -29,10 +29,10 @@ const words = [
 ];
 
 const counterItems = [
-  { value: 15, suffix: "+", label: "Years of Experience" },
-  { value: 200, suffix: "+", label: "Satisfied Clients" },
-  { value: 108, suffix: "+", label: "Completed Projects" },
-  { value: 90, suffix: "%", label: "Client Retention Rate" },
+  { value: 4, suffix: "+", label: "Years of Learning" },
+  { value: 50, suffix: "+", label: "Personal Projects" },
+  { value: 30, suffix: "+", label: "Technologies Learned" },
+  { value: 100, suffix: "+", label: "DSA Problems Solved" },
 ];
 
 const logoIconsList = [
